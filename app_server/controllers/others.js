a1a199cab1978258f50d4b8b4ee02cbd2f96c283
Miquel Puig <mpuig22@gmail.com>
2016-03-23 14:02:44 +0000

    segon

---
+++ app_server/controllers/others.js
@@ -0,0 +1,8 @@
+module.exports.index = function(req, res){
+    res.render('index', { title: 'Express' });
+};
+
+/* GET 'about' page */
+module.exports.about = function(req, res){
+res.render('index', { title: 'About' });
+};
