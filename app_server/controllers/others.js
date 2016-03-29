32f0e75d8099bce7b44759c4b3e910a78afc233e
Miquel Puig <mpuig22@gmail.com>
2016-03-29 15:56:27 +0000

    Adding the view templates

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -4,5 +4,5 @@ module.exports.index = function(req, res){
 
 /* GET 'about' page */
 module.exports.about = function(req, res){
-res.render('index', { title: 'About' });
+res.render('generic-text', { title: 'About' });
 };
