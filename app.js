7a5037f158f65555894e89668445d155fb87dbe7
Miquel Puig <mpuig22@gmail.com>
2016-06-20 13:48:45 +0000

    Directives fetes

--- app.js
+++ app.js
@@ -27,9 +27,15 @@ var appClientFiles = [
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
   'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'
+  'app_client/common/directives/ratingStars/ratingStars.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/pageHeader/pageHeader.directive.js',
 ];
-var uglified = uglifyJs.minify(appClientFiles, {  compress: false });
+
+var uglified = uglifyJs.minify(appClientFiles, {
+  compress: false
+});
 fs.writeFile('public/angular/loc8r.min.js', uglified.code, function(err) {
   if (err) {
     console.log(err);
@@ -56,6 +62,11 @@ app.use(express.static(path.join(__dirname, 'app_client')));
 //app.use('/', routes);
 app.use('/api', routesApi);
 app.use('/users', users);
+app.use(function(req, res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
